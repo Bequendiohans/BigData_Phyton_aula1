f56f8ce1b908d52fd912287761656c0887a99ef9
Bequendiohans <asafesias@gmail.com>
2025-11-18 20:52:50 -0300

    Inicia arquivo teste.py

---
+++ aula_01/desafio_1.py
@@ -0,0 +1,4 @@
+vendas=float(input('Qual o valor de vendas que você atingiu hoje? '))
+meta=15000
+porcentagem=((vendas/meta)*100)
+print(f'A sua porcentagem atual em relação a meta é: {porcentagem:.2f} %')
