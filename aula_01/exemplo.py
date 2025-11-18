baebf7f9603fa23da27b5c6bd903e417ae763ffd
Bequendiohans <asafesias@gmail.com>
2025-11-18 19:41:48 -0300

    Primeiros passos da primeira aula

---
+++ aula_01/exemplo.py
@@ -0,0 +1,34 @@
+# # nome= "Jennifer"
+# # idade= 29
+# # salario= 7500.0
+# # print(f'O nome dela é {nome}, \nidade {idade}, \nsalario {salario}')
+
+# # cor= input('Qual a sua cor favorita? ')
+# # print(f'Sua cor favorita é {cor}!')
+
+# # n1 = float(input('Informe um número: '))
+# # n2 = float(input('Informe o segundo número: '))
+# # resultado = n1+n2
+# # print(f'O resultado da operação é: {resultado}')   
+
+# # cidade = input('Qual cidade você mora? ') 
+# # bairro = input('Qual o bairro? ')
+
+# # ano=2025
+# # idade=int(input('Qual a sua idade? '))
+# # nascimento=ano-idade
+# # print(f'Você nasceu no ano de: {nascimento}')
+
+# ingresso=36
+# reais=int(input('Quantos reais você tem? '))
+# quant= reais//ingresso
+# print(f'Você consegue comprar {quant} ingressos com esse valor.')
+
+# preco=float(input(f'Informe o preco:'))
+# qtd=int(input('Informe a quantidade: '))
+# total=preco*qtd; desconto=total*0.1; valor_final=total-desconto
+# print(f'Valor total { total:.2f}\nDesconto de 10% {desconto:.2f}\nValor a pagar: {valor_final:.2}')
+
+# sal_liq=float(input('Informe seu salário: R$'))
+# sal_tot=sal_liq+(sal_liq*0.30)
+# print(f'Seu salário com adicional noturno é de: R${sal_tot:.2f}\nEspero ter ajudado!')
